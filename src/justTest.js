13408e5b05ba39fe9e83dcdeb4c3004a5c39515c
Eagle <Summer.p.y@foxmail.com>
2019-12-15 20:56:29 +0800

    坐标完全基于动态。适配更多机型

--- src/justTest.js
+++ src/justTest.js
@@ -1,8 +1,19 @@
 
 let deviceWidth = device.width;
 let deviceHeight = device.height;
+
 console.log("开始")
 
-textContains("去浏览").findOnce().click();
+
+// swipe(deviceWidth / 2, deviceHeight / 2, deviceWidth / 2, 0, 1000);
+// let rvListChilds = className("android.support.v7.widget.RecyclerView").findOnce().children();
+// className("android.support.v7.widget.RecyclerView").findOnce().children().forEach(function (child) {
+//     log("孩子们", child.bounds());
+// });
+
+// let contain = className("android.view.View").descContains("好店签到").findOnce().bounds();
+
+swipe(deviceWidth / 2, 680, deviceWidth / 2, 0, 1000);
+
 
 console.log("结束")
