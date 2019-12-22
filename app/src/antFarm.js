0f501ba9f6d8d461666c5f1003f151ca6476f359
Eagle <Summer.p.y@foxmail.com>
2019-12-22 11:40:01 +0800

    农场时候判断小鸡是否外出

--- app/src/antFarm.js
+++ app/src/antFarm.js
@@ -4,35 +4,12 @@ let deviceHeight = device.height;
 function startFarm() {
     //进入支付宝
     //判断当前是否进入了蚂蚁庄园
-    let antFarm = className("android.webkit.WebView").text("蚂蚁庄园").findOnce();
+    let antFarm = className("android.webkit.WebView").textContains("蚂蚁庄园").findOnce();
     if (antFarm != null) {
         //判断自己家的鸡在不在
-        
-        //收蛋
-        toastLog("查看是否有贼鸡！")
-        let screenZj = getScreenImg();
-        let zeiren = images.read("./res/zei_chicken.png");
-        let pList = images.matchTemplate(screenZj, zeiren, { threshold: 0.8, region: [Math.round(deviceWidth * 0.185), deviceHeight * 0.63], max: 2 });
-        if (pList != null) {
-            console.log("找到的贼鸡", pList.matches);
-            if (pList.matches != null && pList.matches.length > 0) {
-                finded.forEach(item => {
-                    console.log("怼贼鸡", item);
-                    click(item.point.x, item.point.y + 10);
-                    sleep(1000);
-                    let screenFuck = getScreenImg();
-                    let fuckOff = images.read("./res/piss_off.png");
-                    let ppPoint = images.matchTemplate(screenFuck, fuckOff, { threshold: 0.8, region: [0, deviceHeight * 0.58], max: 1 });
-                    if (ppPoint != null && ppPoint.matches != null) {
-                        toastLog("敢走贼鸡");
-                        let pointItem = ppPoint.matches[0];
-                        click(pointItem.point.x, pointItem.point.y);
-                        sleep(1000);
-                    }
-                });
-            }
-        }
-        toastLog("没有贼鸡，喂饲料,并收蛋");
+        juadgeSlefAtHome();
+        //判断是否有贼鸡
+        juadgeHasZeiji();
         //喂饲料 930 2000
         click(Math.round(deviceWidth * 0.861), Math.round(deviceHeight * 0.9));
         sleep(500);
@@ -45,6 +22,61 @@ function startFarm() {
     }
     toastLog("完事收工");
     exit();
+}
+
+function juadgeSlefAtHome() {
+    let screenChickenO = getScreenImg();
+    let chickenOut = images.read("./res/chicken_out.png");
+    let chickenOPoint = images.matchTemplate(screenChickenO, chickenOut, { threshold: 0.8, region: [deviceWidth / 3, deviceHeight / 2, deviceWidth / 3, deviceHeight * 0.18], max: 1 }).matches;
+    if (chickenOPoint != null && chickenOPoint.length > 0) {
+        let findPoint = chickenOPoint[0];
+        click(findPoint.point.x, findPoint.point.y);
+        sleep(2000);
+        // 找自己的鸡
+        click(deviceWidth * 0.37, deviceHeight * 0.74);
+        sleep(1000);
+        // 判断是否点错了
+        let imgScrrenPop = getScreenImg();
+        let popNote = images.findMultiColors(imgScrrenPop, "#CCCCCC", [[20, 20, "#CCCCCC"], [deviceWidth * 0.39, 20, "#ff3c45"]], {
+            region: [deviceWidth * 0.12, deviceHeight * 0.39, deviceWidth * 0.76, deviceHeight / 4]
+        });
+        if (popNote != null) {//点错了
+            click(popNote.point.x + 100, popNote.point.y + 50);
+            sleep(1000);
+            click(deviceWidth * 0.79, deviceHeight * 0.74);
+            sleep(2000);
+        }else{
+            sleep(1000);
+        }//没有 直接回家了
+        toastLog("回家了");
+    }
+}
+
+
+function juadgeHasZeiji() {
+    toastLog("查看是否有贼鸡！")
+    let screenZj = getScreenImg();
+    let zeiren = images.read("./res/zei_chicken.png");
+    let pList = images.matchTemplate(screenZj, zeiren, { threshold: 0.8, region: [Math.round(deviceWidth * 0.185), deviceHeight * 0.63], max: 2 });
+    if (pList != null) {
+        console.log("找到的贼鸡", pList.matches);
+        if (pList.matches != null && pList.matches.length > 0) {
+            pList.matches.forEach(item => {
+                console.log("怼贼鸡", item);
+                click(item.point.x, item.point.y + 10);
+                sleep(1000);
+                let screenFuck = getScreenImg();
+                let fuckOff = images.read("./res/piss_off.png");
+                let ppPoint = images.matchTemplate(screenFuck, fuckOff, { threshold: 0.8, region: [0, deviceHeight * 0.58], max: 1 });
+                if (ppPoint != null && ppPoint.matches != null) {
+                    toastLog("贼鸡拿命来");
+                    let pointItem = ppPoint.matches[0];
+                    click(pointItem.point.x, pointItem.point.y);
+                    sleep(2000);
+                }
+            });
+        }
+    }
 }
 
 /**
@@ -61,5 +93,6 @@ function getScreenImg() {
         return screenPic;
     }
 }
+// startFarm();
 
 module.exports = startFarm;
--- app/src/justTest.js
+++ app/src/justTest.js
@@ -3,47 +3,25 @@ let deviceWidth = device.width;
 let deviceHeight = device.height;
 
 console.log("开始")
-function getNeedGetAliScore(need) {
-    //如果需要领取积分
-    if (need) {
-        let inMine = id("tab_description").text("我的").findOnce();
-        click(inMine.bounds().centerX(), inMine.bounds().centerY());
-        sleep(500);
-        //判断当前有没有积分可以领取
-        let hasScore = textContains("个积分待领取").findOnce();
-        if (hasScore != null) {
-            let backCount = 0;
-            let memeberAli = id("item_left_text").text("支付宝会员").findOnce();
-            if (memeberAli != null) {
-                click(memeberAli.bounds().centerX(), memeberAli.bounds().centerY());
-                backCount++;
-                sleep(2000);
-                let getScoreV = className("android.view.View").text("领积分").findOnce();
-                if (getScoreV != null) {
-                    click(getScoreV.bounds().centerX(), getScoreV.bounds().centerY());
-                    sleep(2000);
-                    backCount++;
-                    let clickGetScore = className("android.view.View").text("点击领取").findOnce();
-                    while (clickGetScore != null) {
-                        click(clickGetScore.bounds().centerX(), clickGetScore.bounds().centerY());
-                        sleep(300);
-                        clickGetScore = className("android.view.View").text("点击领取").findOnce();
-                    }
-                }
-            }
 
-            for (let i = 0; i < backCount; i++) {
-                back();
-                sleep(500);
-            }
-        } else {
-            toastLog("没有积分可领取")
-        }
-        let inHome = id("tab_description").text("首页").findOnce();
-        click(inHome.bounds().centerX(), inHome.bounds().centerY());
-        sleep(500);
+
+
+/**
+ * 获取屏幕图片
+ */
+
+function getScreenImg() {
+    requestScreenCapture();
+    let screenPic = captureScreen();
+    console.log(screenPic);
+    sleep(100);
+    if (screenPic == null || typeof (screenPic) == "undifined") {
+        toastLog("截图失败,退出脚本");
+        exit();
+    } else {
+        return screenPic;
     }
 }
-
-getNeedGetAliScore(true);
-console.log("结束")
+// let antFarm = className("android.webkit.WebView").textContains("蚂蚁庄园").findOnce();
+startFarm();
+console.log("结束", antFarm)
